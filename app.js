9306fe534502f43816ca87ab45b42845a848a803
Bruno Peixoto <brunolnetto@gmail.com>
2022-11-30 19:36:32 -0300

    refac/ move routes to routes/

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ import express from 'express';
 import logger from 'morgan';
 import cookieParser from 'cookie-parser';
 
+import router from './routes/routes.js';
+
 const app = express();
 
 // Middlewares
@@ -10,41 +12,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-// Routes
-let users = [];
-
-app.get('/', (req, res) => {
-    res.send(users)
-})
-
-app.post('/user', (req, res) => {
-    users.push(req.body);
-    res.send(users)
-})
-
-app.get('/user/:id', (req, res) => {
-    const userById = users.find(user => user.id === req.params.id);
-    if(userById) {
-        res.send(userById)
-    } else {
-        res.status(400).send({
-            message: 'No user found with given ID'
-        })
-    }
-})
-
-app.put('/user/:id', (req, res) => {
-    const userByIdIndex = users.findIndex(user => user.id === req.params.id);
-    if(userByIdIndex !== -1) {
-        const userById = { ...users[userByIdIndex], ...req.body }
-        users[userByIdIndex] = userById;
-        res.send(users[userByIdIndex])
-    } else {
-        res.status(400).send({
-             message: 'No user found with given ID'
-        })
-    }
-})
+app.use(router);
 
 // register custom error handling middleware
 app.use(
---
+++ routes/routes.js
@@ -0,0 +1,41 @@
+import express from 'express';
+
+let router = express.Router();
+
+// Routes
+let users = [];
+
+router.get('/', (req, res) => {
+    res.send(users)
+})
+
+router.post('/user', (req, res) => {
+    users.push(req.body);
+    res.send(users)
+})
+
+router.get('/user/:id', (req, res) => {
+    const userById = users.find(user => user.id === req.params.id);
+    if(userById) {
+        res.send(userById)
+    } else {
+        res.status(400).send({
+            message: 'No user found with given ID'
+        })
+    }
+})
+
+router.put('/user/:id', (req, res) => {
+    const userByIdIndex = users.findIndex(user => user.id === req.params.id);
+    if(userByIdIndex !== -1) {
+        const userById = { ...users[userByIdIndex], ...req.body }
+        users[userByIdIndex] = userById;
+        res.send(users[userByIdIndex])
+    } else {
+        res.status(400).send({
+             message: 'No user found with given ID'
+        })
+    }
+})
+
+export default router;
